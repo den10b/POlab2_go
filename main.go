39d4ee8c3568af29ee2161f9da9d44f7aa4401d6
den10b <den10b@yandex.ru>
2023-10-28 11:58:43 +0300

    fix

--- main.go
+++ main.go
@@ -16,9 +16,8 @@ func main() {
 		fmt.Printf("Ошибка ввода %v\n", err)
 	}
 
-	beginTime := time.Now()
 	resArr := new([]string)
-	enumerateStrings("", 5, resArr)
+	enumerateStrings("", 5, resArr) // Создаём список всех возможных комбинаций
 	toFind := []string{
 		"1115dd800feaacefdf481f1f9070374a2a81e27880f187396db67958b207cbad",
 		"3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b",
@@ -29,13 +28,21 @@ func main() {
 	useAsync := true
 	resChan := make(chan string, len(toFind))
 	funcWG := new(sync.WaitGroup)
-
+	beginTime := time.Now()
 	for i := 0; i < nRoutines; i++ {
 		funcWG.Add(1)
 		if useAsync {
-			go brute(resArr, i*len(*resArr)/nRoutines, (i+1)*len(*resArr)/nRoutines, &toFind, &resChan, funcWG)
+			// асинхронный запуск горутины
+			go brute(resArr,
+				i*len(*resArr)/nRoutines,
+				(i+1)*len(*resArr)/nRoutines,
+				&toFind, &resChan, funcWG)
 		} else {
-			brute(resArr, i*len(*resArr)/nRoutines, (i+1)*len(*resArr)/nRoutines, &toFind, &resChan, funcWG)
+			// вызов функции, с ожиданием её выполнения
+			brute(resArr,
+				i*len(*resArr)/nRoutines,
+				(i+1)*len(*resArr)/nRoutines,
+				&toFind, &resChan, funcWG)
 		}
 	}
 	funcWG.Wait()
@@ -46,6 +53,7 @@ func main() {
 	fmt.Println("Время выполнения: ", time.Since(beginTime))
 }
 
+// brute
 func brute(passws *[]string, bI, eI int, toFind *[]string, resChan *chan string, wg *sync.WaitGroup) {
 	for i := bI; i < eI; i++ {
 		shaCode := sha256.Sum256([]byte((*passws)[i]))
@@ -58,6 +66,8 @@ func brute(passws *[]string, bI, eI int, toFind *[]string, resChan *chan string,
 	}
 	wg.Done()
 }
+
+// enumerateStrings перебираент строки заданной длинны
 func enumerateStrings(prefix string, length int, resArr *[]string) {
 	if length == 0 {
 		*resArr = append(*resArr, prefix)
